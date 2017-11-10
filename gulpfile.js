825cdabcb07cbb803d3c095ebfc75b23e6e3a1d9
Bogdan Muranets <bogdan.muranets@gmail.com>
2017-11-10 16:34:44 +0200

    docs

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,64 @@
 var browserify = require('browserify'),
-gulp = require('gulp'),
-sourcemaps = require('gulp-sourcemaps'),
-sass = require('gulp-sass'),
-autoprefixer = require('gulp-autoprefixer'),
-source = require('vinyl-source-stream'),
-buffer = require('vinyl-buffer'),
-browserSync = require('browser-sync');
+    gulp = require('gulp'),
+    sourcemaps = require('gulp-sourcemaps'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
+    browserSync = require('browser-sync');
 rename = require('gulp-rename');
 cssmin = require('gulp-cssmin');
 
 /* pathConfig*/
 var entryPoint = './src/index.js',
-browserDir = './',
-sassWatchPath = './styles/**/*.scss',
-jsWatchPath = './src/**/*.js',
-htmlWatchPath = './**/*.html';
+    browserDir = './',
+    sassWatchPath = './styles/**/*.scss',
+    jsWatchPath = './src/**/*.js',
+    htmlWatchPath = './**/*.html';
 /**/
 
 gulp.task('js', function () {
-return browserify(entryPoint, {debug: true, extensions: ['es6']})
-    .transform("babelify", {presets: ["es2015"]})
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./build/'))
-    .pipe(browserSync.reload({stream: true}));
+    return browserify(entryPoint, { debug: true, extensions: ['es6'] })
+        .transform("babelify", { presets: ["es2015"] })
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./build/'))
+        .pipe(browserSync.reload({ stream: true }));
 });
 
 gulp.task('browser-sync', function () {
-const config = {
-    server: {baseDir: browserDir}
-};
+    const config = {
+        server: { baseDir: browserDir }
+    };
 
-return browserSync(config);
+    return browserSync(config);
 });
 
 gulp.task('sass', function () {
-return gulp.src(sassWatchPath)
-.pipe(sass().on('error', sass.logError))
-.pipe(autoprefixer({
-    browsers: ['last 2 versions']
-}))
-.pipe(gulp.dest('./build/css'))
-.pipe(cssmin())
-.pipe(rename({ suffix: '.min' }))
-.pipe(gulp.dest('./build/css-min'))
-.pipe(browserSync.reload({stream: true}));
+    return gulp.src(sassWatchPath)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(gulp.dest('./build/css'))
+        .pipe(cssmin())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('./build/css-min'))
+        .pipe(browserSync.reload({ stream: true }));
 });
 
 gulp.task('watch', function () {
-gulp.watch(jsWatchPath, ['js']);
-gulp.watch(sassWatchPath, ['sass']);
-gulp.watch(htmlWatchPath, function () {
-    return gulp.src('')
-        .pipe(browserSync.reload({stream: true}))
-});
+    gulp.watch(jsWatchPath, ['js', reload]);
+    gulp.watch(sassWatchPath, ['sass', reload]);
+    gulp.watch(htmlWatchPath, reload);
 });
 
 gulp.task('run', ['js', 'sass', 'watch', 'browser-sync']);
+
+function reload() {
+    return gulp.src('')
+        .pipe(browserSync.reload({ stream: true }))
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
-  $('.top-section__scroll-icon').click(function(event) {
-    $("html, body").animate({ scrollTop: $('.desc-section').offset().top }, 1000);
+  $('.top-section__scroll-btn').click(function(event) {
+    $("html, body").animate({ scrollTop: $('.second-section').offset().top }, 1000);
   });
-  $('.in-addition-section__scroll-icon').click(function(event) {
-    $("html, body").animate({ scrollTop: $('.why-section').offset().top }, 1000);
+  $('.second-section__scroll-btn').click(function(event) {
+    $("html, body").animate({ scrollTop: $('.form-section').offset().top }, 1000);
+  });
+
+  $('.register-form__country-select').chosen({
+    width: "100%"
+  });
+  $('.register-form__multi-select').chosen({
+    width: "100%"
   });
 });
